8ca51093180de0fb2cd76dc7953c210c71da269f
Даниил Лямин <Niger120404@yandex.ru>
2022-10-14 23:39:21 +0300

    Добавьте файлы проекта.

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Notes.ViewModel;
+using System.Text;
+
+namespace Notes
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage(MainViewModel mainViewModel)
+		{
+			InitializeComponent();
+			BindingContext = mainViewModel;
+		}
+	}
+}
---
+++ Notes/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Text;
+
+namespace Notes.ViewModel
+{
+	public partial class MainViewModel : ObservableObject
+	{
+		public MainViewModel()
+		{
+			Items = new ObservableCollection<string>();
+		}
+
+		[ObservableProperty]
+		private ObservableCollection<string> _items;
+
+		[ObservableProperty]
+		private string text;
+
+		[RelayCommand]
+		private void Add()
+		{
+			if (string.IsNullOrWhiteSpace(text))
+			{
+				return;
+			}
+			Items.Add(text);
+			Text = string.Empty;
+		}
+
+		[RelayCommand]
+		private void Delete(string s)
+		{
+			if (Items.Contains(s))
+			{
+				Items.Remove(s);
+			}
+		}
+		
+	}
+}
